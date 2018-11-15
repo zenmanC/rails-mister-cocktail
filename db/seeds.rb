5730ec07106d30933aa71594d4455761266ca90f
Chris Cheung <ckwcheung123@icloud.com>
2018-11-15 16:27:37 +0800

    cocktails showing and creating

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "chilis")
+Ingredient.create(name: "martini")
+Ingredient.create(name: "baileys")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "soda water")
