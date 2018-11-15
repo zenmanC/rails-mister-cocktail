08fb127c251cab44de4792ef60c3272268bd50e0
Chris Cheung <ckwcheung123@icloud.com>
2018-11-15 21:25:56 +0800

    routes complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#home'
+    # resources :cocktails do
+    #   resources :doses
+    # end
+
     resources :cocktails do
-      resources :doses
+      resource :doses, only: [:new, :create]
     end
 
+    resources :doses, only: [:destroy]
 end
