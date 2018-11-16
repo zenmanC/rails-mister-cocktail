11f9328a996d23a82942a900203465e76867aaa5
Chris Cheung <ckwcheung123@icloud.com>
2018-11-16 13:12:24 +0800

    destroy added to cocktail for convenience

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,10 +21,15 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
 private
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
   end
-
 end
