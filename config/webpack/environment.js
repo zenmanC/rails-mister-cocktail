ae6914c789d71175047e28f13e5e481155143f1e
Chris Cheung <ckwcheung123@icloud.com>
2018-12-08 16:09:25 +0800

    updates made

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
