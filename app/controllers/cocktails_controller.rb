2b2cdd3b12f2cca001daf1f090a0a8cbca32f4d9
Chris Cheung <ckwcheung123@icloud.com>
2018-12-10 13:43:48 +0800

    pushing to Heroku

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,6 +30,6 @@ class CocktailsController < ApplicationController
 private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo, :photo_cache, :photo2, :photo2_cache)
+    params.require(:cocktail).permit(:name, :photo, :photo2)
   end
 end
