2d4623eb6e3ce1669f169a52809887c49b111cc1
Chris Cheung <ckwcheung123@icloud.com>
2018-12-11 17:23:45 +0800

    seeds updated with real ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.create(name: "lemon")
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "lime")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "rum")
-Ingredient.create(name: "whiskey")
-Ingredient.create(name: "coke")
-Ingredient.create(name: "chilis")
-Ingredient.create(name: "martini")
-Ingredient.create(name: "baileys")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "soda water")
+p "destroying previous ingredients"
+
+Ingredient.delete_all
+
+p "creating new indgredients"
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+# puts "#{drinks['strIngredient1']}"
+
+sorted_ingredients = ingredients['drinks'].sort{|a,b| a['strIngredient1']<=>b['strIngredient1']}
+
+sorted_ingredients.each do |item|
+  Ingredient.create(name: item['strIngredient1'])
+end
+
+p "ingredients complete"
